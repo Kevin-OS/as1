972a97c1a2b3043ce2987891fa343b9ce6a46538
U-Kevin-PC\Kevin <kevpcmaint@hotmail.co.uk>
2017-02-08 19:20:03 +0000

    Added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+#declaring the variables
+name = "Kevin"
+surname = "O' Sullivan"
+num1 = 10
+num2 = 3
+
+#printing a line of text
+#concatenating two string variables
+#printing the data type of the "num1" variable
+#adding two integer variables and changing result to a string variable
+#cannot concatenate a string and an integer
+print ("Hello World!")
+print (name + " " + surname)
+print (type(num1))
+print ("This is the number " + str(num1 + num2) + ".")
